2171a5cd04249221bfc153c9aa894b5ab83e9f3a
Elena Winters <elenamaywinters@gmail.com>
2023-05-10 19:19:47 -0500

    Change export format

--- shared/u_common.js
+++ shared/u_common.js
@@ -1,7 +1,13 @@
 const Delay = (ms) => new Promise(res => setTimeout(res, ms));
 
-exports("math0round", (number, place) => {
-    if (number == 0) { return 0; }
-    let pow = (place === undefined) ? 1 : place;
-    return Number(Math.round((number + Number.EPSILON) + 'e' + pow) + 'e-' + pow)
+exports("math", () => {
+    return {
+        round: function test(number, place) {
+            if (number == 0) { return 0; }
+            let pow = (place === undefined) ? 1 : place;
+            return Number(Math.round((number + Number.EPSILON) + 'e' + pow) + 'e-' + pow)
+        }
+    }
 });
+
+console.log(exports.twiliCore.math().round(10.1346821676, 3))
