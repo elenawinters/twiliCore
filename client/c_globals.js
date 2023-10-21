946df13368d897a15a7e2ff29c3719ed86f37c36
Elena Winters <elenamaywinters@gmail.com>
2023-10-20 19:45:52 -0500

    Revert to reliable damage calculations

--- client/c_globals.js
+++ client/c_globals.js
@@ -10,11 +10,11 @@ const PLAYER_INDEX = NetworkGetPlayerIndexFromPed(PLAYER_PED());
 const PLAYER_NETID = GetPlayerServerId(PLAYER_INDEX);
 
 
-exports("BUILD", () => { return BUILD; });
-exports("GAME", () => { return GAME; });
-exports("FIVEM", () => { return FIVEM; });
-exports("REDM", () => { return REDM; });
-exports("PLAYER_PED", PLAYER_PED);
-exports("PLAYER_INDEX", () => { return PLAYER_INDEX; });
-exports("PLAYER_NETID", () => { return PLAYER_NETID; });
+// exports("BUILD", () => { return BUILD; });
+// exports("GAME", () => { return GAME; });
+// exports("FIVEM", () => { return FIVEM; });
+// exports("REDM", () => { return REDM; });
+// exports("PLAYER_PED", PLAYER_PED);
+// exports("PLAYER_INDEX", () => { return PLAYER_INDEX; });
+// exports("PLAYER_NETID", () => { return PLAYER_NETID; });
 
--- client/c_tests.js
+++ client/c_tests.js
@@ -1,6 +1,6 @@
 
+// This doesn't work?
+// onNet('twiliCore:damage:event', (suspect, victim, situation) => {
+//     console.log(`damage:event ${JSON.stringify(suspect)} -> ${JSON.stringify(victim)} /w ${JSON.stringify(situation)}`);
 
-onNet('twiliCore:damage:event', (suspect, victim, situation) => {
-    console.log(`damage:event ${JSON.stringify(suspect)} -> ${JSON.stringify(victim)} /w ${JSON.stringify(situation)}`);
-
-})
+// })
